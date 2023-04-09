91295c808d32f479a2a16b71b2e3a7bd641cb4a1
Balan Sebastian <76501752+balansebastianm@users.noreply.github.com>
2023-04-10 01:21:57 +0300

    Add 404 page.

--- LicentaFinal/Program.cs
+++ LicentaFinal/Program.cs
@@ -49,7 +49,7 @@ if(args.Length == 1 && args[0].ToLower() == "seeddata")
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
